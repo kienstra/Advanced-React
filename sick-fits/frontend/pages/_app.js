c02ab0ef8c7c3b598c8e1bb4fd44e5327de7281e
Ryan Kienstra <kienstraryan@gmail.com>
2021-02-07 12:02:32 -0600

    Use NProgress as a progress bar

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,6 +1,13 @@
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
+import '../components/styles/nprogress.css';
 
-export default function MyApp({ Component, pageProps}) {
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
+export default function MyApp({ Component, pageProps }) {
   return (
     <Page>
       <Component {...pageProps} />
