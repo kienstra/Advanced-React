ea06221920cb967b6b0af408aa44c96c58f9eb4f
Ryan Kienstra <kienstraryan@gmail.com>
2021-02-07 16:02:21 -0600

    Allow creating a first user to log into Keystone.js

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,15 +1,27 @@
+import { createAuth } from '@keystone-next/auth';
 import { config, createSchema } from '@keystone-next/keystone/schema'
 import { User } from './schemas/User';
 import 'dotenv/config';
+import { statelessSessions, withItemData } from '@keystone-next/keystone/session';
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial'
 
 const sessionConfig = {
   maxAge: 60 * 60 * 24 * 360, // How long should they stay signed in?
   secret: process.env.COOKIE_SECRET,
-}
+};
 
-export default config({
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email', // What do they log in with?
+  secretField: 'password',
+  initFirstItem: {
+      fields: ['name', 'email', 'password'],
+      // @todo: Add initial roles here.
+  }
+});
+
+export default withAuth( config({
   server: {
       cors: {
           origin: [process.env.FRONTEND_URL],
@@ -25,8 +37,12 @@ export default config({
       User
   }),
   ui: {
-    // @todo: Change this for roles
-    isAccessAllowed: () => true,
+    // Show UI only for people who meet this requirement.
+    isAccessAllowed: ({ session }) => {
+        return !!session?.data;
+    }
   },
-  // @todo: Add session values.
-});
+  session: withItemData( statelessSessions( sessionConfig), {
+      User: `id`,
+  })
+}));
