2111d5575dc9e0a13f7f3fd807155a33cd20f4e8
Ryan Kienstra <kienstraryan@gmail.com>
2021-02-13 18:24:19 -0600

    Add a <Pagination> component, following Wes's code

---
+++ sick-fits/frontend/components/Pagination.js
@@ -0,0 +1,43 @@
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/client';
+import Head from 'next/head';
+import Link from 'next/link';
+import ErrorMessage from './ErrorMessage';
+import PaginationStyles from './styles/PaginationStyles';
+import { perPage } from '../config';
+
+const PAGINATION_QUERY = gql`
+  query PAGINATION_QUERY {
+    _allProductsMeta {
+      count
+    }
+  }
+`;
+
+export default function Pagination({ page }) {
+  const { error, loading, data } = useQuery(PAGINATION_QUERY);
+
+  if (loading) return null;
+  if (error) return <ErrorMessage error={error} />;
+
+  const { count } = data._allProductsMeta;
+  const totalPageCount = Math.ceil(count / perPage);
+
+  return (
+    <PaginationStyles>
+      <Head>
+        <title>
+          Sick Fits - Page {page} of {totalPageCount}
+        </title>
+      </Head>
+      <Link href={`/products/${page - 1}`}>
+        <a aria-disabled={page === 1}>← Prev</a>
+      </Link>
+      <p>Page __ of {totalPageCount}</p>
+      <p>{count} Items Total</p>
+      <Link href={`/products/${page + 1}`}>
+        <a aria-disabled={page >= totalPageCount}>Next →</a>
+      </Link>
+    </PaginationStyles>
+  );
+}
