dd6453e3b11ba49eea4295e24fd63890d59ddb77
Ryan Kienstra <kienstraryan@gmail.com>
2021-02-14 23:38:28 -0600

    Add a button to sign out

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
 import { useUser } from './User';
+import SignOut from './SignOut';
 
 export default function Nav() {
   const user = useUser();
@@ -13,9 +14,10 @@ export default function Nav() {
           <Link href="/sell">Sell</Link>
           <Link href="/orders">Orders</Link>
           <Link href="/account">Account</Link>
+          <SignOut />
         </>
       ) : (
-        <Link href="/sign-in">Sign In</Link>
+        <Link href="/signin">Sign In</Link>
       )}
     </NavStyles>
   );
