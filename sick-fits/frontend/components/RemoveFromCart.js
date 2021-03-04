8babdb0c8af6bdd2d876d274e82125bb3ddec9c1
Ryan Kienstra <kienstraryan@gmail.com>
2021-03-03 23:46:14 -0600

    Apply Wes' suggestions, including styling of the button

--- sick-fits/frontend/components/RemoveFromCart.js
+++ sick-fits/frontend/components/RemoveFromCart.js
@@ -1,6 +1,17 @@
 import gql from 'graphql-tag';
 import { useMutation } from '@apollo/client';
+import styled from 'styled-components';
 import { CURRENT_USER_QUERY } from './User';
+
+const BigButton = styled.button`
+  font-size: 3rem;
+  background: none;
+  border: 0;
+  &:hover {
+    color: var(--red);
+    cursor: pointer;
+  }
+`;
 
 const REMOVE_FROM_CART_MUTATION = gql`
   mutation REMOVE_FROM_CART_MUTATION($id: ID!) {
@@ -10,21 +21,24 @@ const REMOVE_FROM_CART_MUTATION = gql`
   }
 `;
 
+function update(cache, payload) {
+  cache.evict(cache.identify(payload.data.deleteCartItem));
+}
+
 export default function RemoveFromCart({ id }) {
   const [removeFromCart, { loading }] = useMutation(REMOVE_FROM_CART_MUTATION, {
     variables: { id },
-    refetchQueries: [{ query: CURRENT_USER_QUERY }],
+    update,
   });
 
   return (
-    <button
-      disabled={loading}
+    <BigButton
       type="button"
-      onClick={() => {
-        removeFromCart();
-      }}
+      disabled={loading}
+      title="Remove This Item From Cart"
+      onClick={removeFromCart}
     >
-      Remove From Cart 🛒
-    </button>
+      &times;
+    </BigButton>
   );
 }
