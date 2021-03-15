18ebd4dbf82c6752ada02cc218ff6c49a1fe3db1
Ryan Kienstra <kienstraryan@gmail.com>
2021-03-15 00:50:30 -0600

    Add the permissions that Wes gave

--- sick-fits/backend/access.ts
+++ sick-fits/backend/access.ts
@@ -1,82 +1,66 @@
 import { permissionsList } from './schemas/fields';
 import { ListAccessArgs } from './types';
-// At it's simplest, the access control returns a yes or no value depending on the users session
 
 export function isSignedIn({ session }: ListAccessArgs) {
   return !!session;
 }
 
-const generatedPermissions = Object.fromEntries(
-  permissionsList.map((permission) => [
-    permission,
-    function ({ session }: ListAccessArgs) {
-      return !!session?.data.role?.[permission];
-    },
-  ])
-);
+const generatedPermissions = Object.fromEntries(permissionsList.map((permission) => [
+  permission,
+  function({session}: ListAccessArgs) {
+    return !!session?.data.role?.[permission];
+  }
+]));
 
-// Permissions check if someone meets a criteria - yes or no.
 export const permissions = {
   ...generatedPermissions,
-  isAwesome({ session }: ListAccessArgs): boolean {
-    return session?.data.name.includes('wes');
-  },
 };
 
-// Rule based function
-// Rules can return a boolean - yes or no - or a filter which limits which products they can CRUD.
+// Rule based functions
+// Can return a boolean or a filter that limits what they can access.
 export const rules = {
-  canManageProducts({ session }: ListAccessArgs) {
-    if (!isSignedIn({ session })) {
-      return false;
-    }
-    // 1. Do they have the permission of canManageProducts
-    if (permissions.canManageProducts({ session })) {
+  canManageProducts({session}: ListAccessArgs) {
+    if (permissions.canManageProducts({session})) {
       return true;
     }
-    // 2. If not, do they own this item?
+
     return { user: { id: session.itemId } };
   },
-  canOrder({ session }: ListAccessArgs) {
-    if (!isSignedIn({ session })) {
+  canOrder({session}: ListAccessArgs) {
+    if (!isSignedIn({session})) {
       return false;
     }
-    // 1. Do they have the permission of canManageProducts
-    if (permissions.canManageCart({ session })) {
+
+    if (permissions.canManageCart({session})) {
       return true;
     }
-    // 2. If not, do they own this item?
+
     return { user: { id: session.itemId } };
   },
-  canManageOrderItems({ session }: ListAccessArgs) {
-    if (!isSignedIn({ session })) {
-      return false;
-    }
-    // 1. Do they have the permission of canManageProducts
-    if (permissions.canManageCart({ session })) {
+  canManageOrderItems({session}: ListAccessArgs) {
+    if (permissions.canManageCart({session})) {
       return true;
     }
-    // 2. If not, do they own this item?
+
     return { order: { user: { id: session.itemId } } };
   },
-  canReadProducts({ session }: ListAccessArgs) {
-    if (!isSignedIn({ session })) {
-      return false;
-    }
-    if (permissions.canManageProducts({ session })) {
-      return true; // They can read everything!
-    }
-    // They should only see available products (based on the status field)
-    return { status: 'AVAILABLE' };
-  },
-  canManageUsers({ session }: ListAccessArgs) {
-    if (!isSignedIn({ session })) {
-      return false;
-    }
-    if (permissions.canManageUsers({ session })) {
+  canReadProducts({session}: ListAccessArgs) {
+    if (permissions.canMangeProducts) {
       return true;
     }
-    // Otherwise they may only update themselves!
+
+    return { status: 'AVAILABLE' };
+  },
+  canManageUsers({session}: ListAccessArgs) {
+    if (!isSignedIn({session})) {
+      return false;
+    }
+
+    if (permissions.canManageUsers({session})) {
+      return true;
+    }
+
+    // Otherwise they may only update themselves.
     return { id: session.itemId };
   },
 };
--- sick-fits/backend/schemas/User.ts
+++ sick-fits/backend/schemas/User.ts
@@ -1,7 +1,19 @@
 import { list } from '@keystone-next/keystone/schema';
 import { text, password, relationship } from '@keystone-next/fields'
+import { permissions, rules } from '../access';
 
 export const User = list({
+  access: {
+    create: () => true,
+    read: rules.canManageUsers,
+    update: rules.canManageUsers,
+    // Only people with permission can delete themselves, you can't delete yourself.
+    delete: permissions.canManageUsers,
+  },
+  ui: {
+    hideCreate: (args) => !permissions.canManageUsers(args),
+    hideDelete: (args) => !permissions.canManageUsers(args),
+  },
   fields: {
     name: text({ isRequired: true }),
     email: text({isRequired: true, isUnique: true }),
@@ -16,8 +28,15 @@ export const User = list({
     }),
     orders: relationship({ ref: 'Order.user', many: true }),
     role: relationship({
-      ref: 'Role.assignedTo'
-      // TODO: add access control
+      ref: 'Role.assignedTo',
+      access: {
+        create: permissions.canManageUsers,
+        update: permissions.canManageUsers,
+      },
+    }),
+    products: relationship({
+      ref: 'Product.user',
+      many: true,
     }),
   },
 });
